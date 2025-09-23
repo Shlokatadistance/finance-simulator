03b9c949b61e70766bf097b7e26a3dfe2b859696
Shlok Sinha <ssinha@Shloks-Mac-mini.local>
2025-09-24 07:54:07 +0800

    Init: Adding the baseline file

---
+++ core.cpp
@@ -0,0 +1,40 @@
+
+#include <iostream>
+#include <iomanip>
+using std::string;
+
+class AbstractFinancialStatements{
+    virtual double returnNetWorth() = 0;
+};
+
+
+class FinancialSheet : AbstractFinancialStatements{
+    public:
+        string Name;
+        string Occupation;
+        double Salary;
+        double NetWorth;
+        double NetLiabilities;
+        int Age;
+
+        FinancialSheet(string name,string occupation,double salary,double netliabilities, int age){
+            Name = name;
+            Occupation = occupation;
+            Salary = salary;
+            NetLiabilities = netliabilities;
+            Age = age;
+        }
+
+        double returnNetWorth(){
+            NetWorth = Salary - NetLiabilities;
+            return NetWorth;
+        }
+};
+
+
+int main(){
+    FinancialSheet me = FinancialSheet("zyn","swe",120000.85,50000.1,24);
+    double currentNetWorth =  me.returnNetWorth();
+    std::cout << me.Name << " is worth "<< std::fixed << std::setprecision(2) << currentNetWorth << std::endl;
+    return 0;
+}
